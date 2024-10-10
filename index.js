6f4c731b2789522f48ab23a5dc9969871eedf377
Autumn Lydon <autumnlydon@gmail.com>
2024-10-10 10:55:42 -0400

    all done :)

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 //write your code here
+
+//Problem 1
+
+const reverseString = (str) => {
+  let newStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr += str[i];
+  }
+  return newStr;
+};
+
+//Problem 2
+
+const reverseZigZagString = (str) => {
+  let newStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    if (i % 2 === 0) {
+      newStr += str[i].toLowerCase();
+    } else {
+      newStr += str[i].toUpperCase();
+    }
+  }
+  return newStr;
+};
